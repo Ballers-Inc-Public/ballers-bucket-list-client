6b6a72a3c5779eb0f2e46aae5a744409208c9bac
brianedmunds <brian.edmundsjr@outlook.com>
2017-05-16 13:58:41 -0400

    Get goals and create goal
    Within the api.js, events.js, and ui.js files, created functions
    to handle the getting and creation of goals.

--- assets/scripts/goals/events.js
+++ assets/scripts/goals/events.js
@@ -11,72 +11,31 @@ const onGetGoals = function (event) {
 
 // API request for index all
   api.signUp(data)
-  .then(ui.signUpSuccess)
-  .catch(ui.signUpFailure)
-}
-
-// Pull single goal___________________
-const onGetSingleGoal = function (event) {
-  event.preventDefault()
-  // console.log('Sign In run')
-  const data = getFormFields(this)
-
-  api.signIn(data)
-    .then(ui.signInSuccess)
-    .catch(ui.signInFailure)
+  .then(ui.getGoalsSuccess)
+  .catch(ui.Failure)
 }
 
 // On Create Goal
 const onCreateGoal = function (event) {
   event.preventDefault()
-  // console.log('Sign out run')
   if (store.user === undefined) {
     // console.log('Not signed In')
     return
   }
-  api.signOut()
-    .then(ui.signOutSuccess)
-    .catch(ui.signOutFailure)
-}
-
-
-// On Update goal___________________
-const onUpdateGoal = function (event) {
-  event.preventDefault()
-  // console.log('Changing password run')
-  const data = getFormFields(this)
-
-  if (
-    data.passwords.old === '' || data.passwords.new === '') {
-    $('#change-pass-blank-field-failure-alert').show()
-    // console.log('No blank fields accepted')
-    return
-  }
-
-  if (
-    data.passwords.old === data.passwords.new) {
-    $('#change-pass-same-password-failure-alert').show()
-    // console.log('same password')
-    return
-  }
-
-  api.changePassword(data)
-    .then(ui.changePasswordSuccess)
-    .catch(ui.changePasswordFailure)
-}
-
-// On Delete Goal
-const onDeleteGoal = function (event) {
-  event.preventDefault()
   // console.log('Sign out run')
-  if (store.user === undefined) {
-    // console.log('Not signed In')
-    return
+  const userId = store.user.id
+  const data = {
+    goal: {
+      title: data.title,
+      status: status,
+      user_id: userId
+    }
   }
-  api.signOut()
-    .then(ui.signOutSuccess)
-    .catch(ui.signOutFailure)
+  api.createGoal(data)
+    .then(ui.createGoalSuccess)
+    .catch(ui.Failure)
 }
+
 // HANDLER TO ASSIGN AUTHORIZATION FUNCTIONS TO OBJECTS___________________
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
--- assets/scripts/goals/ui.js
+++ assets/scripts/goals/ui.js
@@ -3,71 +3,15 @@
 const store = require('../store')
 
 // Sign UP SUCCESS AND FAILURE MESSAGING ________________________
-const signUpSuccess = (data) => {
-  console.log('User sucessfully created:', data)
-  console.log('Store looks like ', store)
-  $('#sign-up').trigger('reset')
+const getGoalsSuccess = (data) => {
+  console.log('Get goals was successful')
 }
 
-const signUpFailure = (error) => {
-  if (error.error.responseText === '{"email":["has already been taken"]}') {
-    console.log('email already taken')
-    return
-  }
-  console.log('Store looks like ', store)
-}
-
-//  SIGN IN SUCCESS AND FAILURE MESSAGING ________________________
-const signInSuccess = (data) => {
-  console.log('signIn success ran, data is: ', data)
-  store.user = data.user
-  console.log('Store looks like ', store)
-  $('#sign-in').trigger('reset')
-}
-
-const signInFailure = (error) => {
-  console.error('signIn error ran, error is: ', error)
-  console.log(error.statusText)
-  console.log('Store looks like ', store)
-}
-
-//  Change Password SUCCESS AND FAILURE MESSAGING ______________________________
-
-const changePasswordSuccess = (data) => {
-  console.log('Password was succesfully changed, data is: ', data)
-  console.log('Store looks like ', store)
-  $('#change-password').trigger('reset')
-}
-
-const changePasswordFailure = (error) => {
-  console.log('Password was not succesfully changed', error)
-  console.log('Store looks like ', store)
-}
-
-//  SIGN OUT SUCCESS AND FAILURE MESSAGING ______________________
-
-const signOutSuccess = () => {
-  console.log('signOut success ran, and nothing was returned')
-  console.log('Store looks like ', store)
-
-  store.user = null
-  store.user_ratings = null
-
-  console.log('Store looks like ', store)
-}
-
-const signOutFailure = (error) => {
-  console.error('signOut error ran, error is: ', error)
-  console.log('Store looks like ', store)
+const createGoalSuccess = (data) => {
+  console.log('Create goal was successful')
 }
 
 module.exports = {
-  signUpSuccess,
-  signUpFailure,
-  signInSuccess,
-  signInFailure,
-  signOutFailure,
-  signOutSuccess,
-  changePasswordFailure,
-  changePasswordSuccess
+  getGoalsSuccess,
+  createGoalSuccess
 }
