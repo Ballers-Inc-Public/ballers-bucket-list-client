9a02942afab409be96bbc33d577402b60e4692b1
sjruby <samuel.ruby@libertymutual.com>
2017-05-17 11:19:00 -0400

    Created logic to sign-in users after sign-up
    Now when a user sign's up they will not need to also sign in b/c we are
    calling the signup API call

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -28,6 +28,13 @@ const onSignUp = function (event) {
   api.signUp(data)
   .then(ui.signUpSuccess)
   .catch(ui.signUpFailure)
+  // if Sign up works then we will run the sign in API call to skip that step
+  .then(() => {
+    console.log('this is what is passed', data)
+    api.signIn(data)
+      .then(ui.signInSuccess)
+      .catch(ui.signInFailure)
+  })
 }
 
 // SIGNIN FUNTIONALITY LAUNCHED WHEN CLICKED IN MODAL___________________
@@ -43,7 +50,7 @@ const onSignIn = function (event) {
     $('#sign-in-blank-field-failure-alert').show()
     return
   }
-
+  console.log('this is what is passed to sign in', data)
   api.signIn(data)
     .then(ui.signInSuccess)
     .catch(ui.signInFailure)
